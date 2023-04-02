d7c8012ecbdcb725e3b78526bb08aee997d9058b
anvaymishra1 <anvay.mishra@awiros.com>
2023-04-02 19:20:54 +0530

    x

--- frontend/src/handlers/prompts.handlers.js
+++ frontend/src/handlers/prompts.handlers.js
@@ -6,21 +6,30 @@ import example from "../examples/tasks.json"
 
 export const postPrompt = async(payload)=>{
     // let response = example 
-    let response = await client('/sub_tasks', {baseURL:"base",method: 'POST', data: payload})
-    if(response.status=== 200){
-        let data_to_send =  await  response.data
-        return {
-            status:true,
-            data:data_to_send
+    try {
+        let response = await client('/sub_tasks', { baseURL: "base", method: 'POST', data: payload })
+        if (response.status === 200) {
+            let data_to_send = await response.data
+            return {
+                status: true,
+                data: data_to_send
+            }
         }
-    }
-    else{
-        toast.error(response.data.message)
+        else {
+            toast.error(response.data.message)
+            return {
+                status: false,
+                data: example
+            }
+        }
+    } catch (error) {
+        toast.error('could not process request')
         return {
             status: false,
-            data:example
+            data: example
         }
     }
+    
     // return {
     //     status:true,
     //     data:response
